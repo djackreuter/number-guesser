d8272b5873fbd72f19e5bb50054e3b43939979bd
Jack Reuter <djreuter45@gmail.com>
2018-03-25 16:41:43 -0600

    added functionality

--- Program.cs
+++ Program.cs
@@ -10,6 +10,90 @@ namespace number_guesser
     {
         static void Main(string[] args)
         {
+            GetAppInfo();
+
+            GreetUser();
+
+        
+            while (true)
+            {
+                // create random number
+                Random random = new Random();
+                int correctNumber = random.Next(1, 11);
+                int guess = 0;
+
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    // make sure user input is a number
+                    if (!int.TryParse(input, out guess))
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Please enter a number");
+                        continue;
+                    }
+
+                    // convert to int
+                    guess = Int32.Parse(input);
+
+                    if (guess != correctNumber)
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "That was incorrect. Guess again...");
+
+                    }
+                }
+
+                PrintColorMessage(ConsoleColor.Yellow, "Correct!!!");
+
+
+                Console.WriteLine("Play again???[Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                } else if (answer == "N")
+                {
+                    return;
+                } else
+                {
+                    return;
+                }
+            }
+        }
+
+        static void GetAppInfo()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jack Reuter";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("{0}: Version: {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+
+            Console.WriteLine("What is your name?");
+
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Thanks {0}! Let\'s get started!", inputName);
+        }
+
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
         }
     }
 }
